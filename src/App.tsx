9d0ec82b20d9d8b1cbcaf48f9da6f9671413e9ad
Ledol <dzmitryliavonenka@gmail.com>
2022-06-30 17:06:42 +0300

    Created Header.tsx, Navbar.tsx, Profile.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,17 @@
 import React from 'react';
 import './App.css';
+import Header from "./components/Header";
+import {Navbar} from "./components/Navbar";
+import Profile from "./components/Profile";
+
 
 
 function App() {
     return (
-        <div className='container'>
-            <header className='header'>
-                <img src="https://media.istockphoto.com/vectors/soccer-ball-symbol-football-ball-icon-vector-id1263601084?k=20&m=1263601084&s=612x612&w=0&h=7Hw0H4ueSCn2hRvjK40dt_cZNk8nVEVXlZhiztVb-cc=" alt="ball"/>
-            </header>
-            <nav className='nav'>
-                <div>
-                    <a href="#">Profile</a>
-                </div>
-                <div>
-                    <a href="#">Message</a>
-                </div>
-                <div>
-                    <a href="#">News</a>
-                </div>
-                <div>
-                    <a href="#">Music</a>
-                </div>
-                <div>
-                    <a href="#">Setting</a>
-                </div>
-            </nav>
-            <div className='content'>
-                <div>
-                    <img src="https://i.pinimg.com/originals/28/23/cc/2823ccecf4f0f0ae3a063069cb160398.png" alt="avatar"/>
-                    <span>Dmitry L</span>
-                </div>
-                <div>
-                    My post
-                </div>
-                <div>
-                    Input and Button
-                </div>
-                <div>
-                    Hey, why nobody
-                </div>
-                <div>
-                    It's our new
-                </div>
-            </div>
+        <div className='app-wrapper'>
+            <Header />
+            <Navbar />
+            <Profile />
         </div>
 
     );
---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => {
+    return <header className='header'>
+        <img
+            src="https://media.istockphoto.com/vectors/soccer-ball-symbol-football-ball-icon-vector-id1263601084?k=20&m=1263601084&s=612x612&w=0&h=7Hw0H4ueSCn2hRvjK40dt_cZNk8nVEVXlZhiztVb-cc="
+            alt="ball"/>
+    </header>
+
+
+};
+
+export default Header;
