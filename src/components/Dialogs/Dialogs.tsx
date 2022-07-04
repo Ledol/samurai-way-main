b3b25bf21366ffb35142a317bdacff4b7226ee81
Ledol <dzmitryliavonenka@gmail.com>
2022-07-04 14:54:50 +0300

    Added style for Dialogs component. Added messages and users in component.

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -3,8 +3,19 @@ import s from './Dialogs.module.css'
 
 const Dialogs = () => {
     return (
-        <div>
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={s.dialog +' '+ s.active}>Dmitry</div>
+                <div className={s.dialog}>Kate</div>
+                <div className={s.dialog}>Alex</div>
+                <div className={s.dialog}>Sveta</div>
+                <div className={s.dialog}>Semen</div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>Hello</div>
+                <div className={s.message}>How are you?</div>
+                <div className={s.message}>What day is it today?</div>
+            </div>
         </div>
     );
 };
