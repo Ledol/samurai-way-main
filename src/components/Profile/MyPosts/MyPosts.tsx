79446879310b94628bf4a5c08962a81e3bd8b521
Ledol <dzmitryliavonenka@gmail.com>
2022-07-10 19:31:23 +0300

    Added 2 Array-data and Refactor code.

--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -2,17 +2,31 @@ import React from 'react';
 import s from './MyPosts.module.css';
 import Post from "./Post/Post";
 
+type PostType = {
+    id: number
+    message: string
+    likesCount: string
+}
 
-const MyPosts = () => {
+
+type MyPostsPropsType = {
+    posts: Array<PostType>
+}
+
+const MyPosts = (props: MyPostsPropsType) => {
+
+    const postsElement = props.posts.map((p) => {
+        return <Post message={p.message}
+                     likesCount={p.likesCount}/>
+    })
+
     return <div className={s.content}>
         <div>My Posts</div>
         <textarea></textarea>
         <div>
             <button>Add post</button>
         </div>
-        <Post message={"Message 1"} likesCount={'like 15'}/>
-        <Post message={"Message 2"} likesCount={'like 8'}/>
-        <Post message={"Message 3"} likesCount={'like 23'}/>
+        { postsElement }
     </div>
 
 };
--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -3,10 +3,17 @@ import style from './Profile.module.css';
 import MyPosts from "./MyPosts/MyPosts";
 import ProfileInfo from "./ProfileInfo/ProfileInfo";
 
+
 function Profile () {
+    const posts = [
+        {id: 1, message: 'Message 1', likesCount: 'like 15'},
+        {id: 2, message: 'Message 2', likesCount: 'like 8'},
+        {id: 3, message: 'Message 3', likesCount: 'like 23'},
+    ]
+
     return <div>
         <ProfileInfo />
-        <MyPosts />
+        <MyPosts posts={posts} />
     </div>
 
 };
